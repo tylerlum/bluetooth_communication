e381712aa51446ed5759ce922c73ee0ed9004fe0
Tyler Lum <tylergwlum@gmail.com>
2020-01-20 20:49:20 -0800

    Add LCD prints to Master to show received values

--- Master/Master.ino
+++ Master/Master.ino
@@ -1,8 +1,19 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// LCD object. Initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 // Pins
 const int LED_PIN = 10;
 
+// Constants
+const int numSamples = 100;
+
 // Global variables
-int brightness = 255;
+int brightness = 0;
+int averageBrightness = 0;
+int averageCount = 0;
 
 void setup() {
   // Setup Serial port
@@ -10,12 +21,30 @@ void setup() {
 
   // Setup LED output pin
   pinMode(LED_PIN, OUTPUT);
+
+  // Setup the LCD's number of columns and rows:
+  lcd.begin(16, 2);
 }
 
 void loop() {
   // Update brightness value, if available
-  if (Serial.available() > 0) { brightness = Serial.read(); } 
+  if (Serial.available() > 0) {
+    brightness = Serial.read();
+  }
 
   // Write to LED
   analogWrite(LED_PIN, brightness);
+
+  // Print averageBrightness every numSamples
+  if (++averageCount == numSamples) {
+    lcd.clear();
+    lcd.print(averageBrightness / numSamples);
+    averageCount = 0;
+    averageBrightness = 0;
+  } 
+
+  // Add up brightness samples
+  else {
+    averageBrightness += brightness;
+  }
 }
