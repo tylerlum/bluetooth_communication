e130d0480091a42e1b917114bac17e0598f1eef1
Tyler Lum <tylergwlum@gmail.com>
2020-03-11 15:05:02 -0700

    Clean up code significantly

--- Slave/Slave.ino
+++ Slave/Slave.ino
@@ -1,43 +1,42 @@
 #include <Wire.h>
 #include <VL53L0X.h>
 
-VL53L0X sensor;
+// Sensors
+VL53L0X sensor1;
 VL53L0X sensor2;
 
 // Pins
 const int PWM_PIN = 10;
 const int DIRECTION_UP_PIN = 9;
-
-const int SENSOR_XSHUT_PIN = 7;
+const int SENSOR_1_XSHUT_PIN = 7;
 const int SENSOR_2_XSHUT_PIN = 8;
 
-void setup()
-{
-  pinMode(SENSOR_XSHUT_PIN, OUTPUT);
+void setupSensors() {
+  // Turn off sensors using XSHUT pins
+  pinMode(SENSOR_1_XSHUT_PIN, OUTPUT);
   pinMode(SENSOR_2_XSHUT_PIN, OUTPUT);
-  digitalWrite(SENSOR_XSHUT_PIN, LOW);
+  digitalWrite(SENSOR_1_XSHUT_PIN, LOW);
   digitalWrite(SENSOR_2_XSHUT_PIN, LOW);
 
-  // Pins
-  pinMode(PWM_PIN, OUTPUT);
-  pinMode(DIRECTION_UP_PIN, OUTPUT);
+  // Setup I2C communication
   delay(500);
   Wire.begin();
 
-  Serial.begin (9600);
+  // Setup Serial for prints
+  Serial.begin(9600);
 
-  //SENSOR
-  pinMode(SENSOR_XSHUT_PIN, INPUT);
+  // Sensor 1 setup
+  pinMode(SENSOR_1_XSHUT_PIN, INPUT);  // Turn on sensor. Can't set OUTPUT HIGH b/c XSHUT can't handle 5V
   delay(150);
   Serial.println("00");
-  sensor.init(true);
+  sensor1.init(true);
   Serial.println("01");
   delay(100);
-  sensor.setAddress((uint8_t)22);
+  sensor1.setAddress((uint8_t)22);
   Serial.println("02");
 
-  //SENSOR 2
-  pinMode(SENSOR_2_XSHUT_PIN, INPUT);
+  // Sensor 2 setup
+  pinMode(SENSOR_2_XSHUT_PIN, INPUT);  // Turn on sensor. Can't set OUTPUT HIGH b/c XSHUT can't handle 5V
   delay(150);
   sensor2.init(true);
   Serial.println("03");
@@ -50,12 +49,20 @@ void setup()
   Serial.println("");
   Serial.println("");
 
-  sensor.setTimeout(500);
+  sensor1.setTimeout(500);
   sensor2.setTimeout(500);
 }
 
-void loop()
+void setup()
 {
+  setupSensors();
+  
+  // Pins
+  pinMode(PWM_PIN, OUTPUT);
+  pinMode(DIRECTION_UP_PIN, OUTPUT);
+}
+
+void checkAddresses() {
   Serial.println("__________________________________________________________________");
   Serial.println("");
   Serial.println("=================================");
@@ -83,15 +90,18 @@ void loop()
   Serial.print (count, DEC);
   Serial.println (" device(s).");
   Serial.println("=================================");
+}
 
+void loop()
+{
+  checkAddresses();
 
   //CHECK DISTANCES
-
-  long upperSensorDistance = (sensor.readRangeSingleMillimeters());
+  long upperSensorDistance = (sensor1.readRangeSingleMillimeters());
   long lowerSensorDistance = (sensor2.readRangeSingleMillimeters());
   
-  //FWD OR SENSOR
-  if (!sensor.timeoutOccurred())
+  // Sensor 1
+  if (!sensor1.timeoutOccurred())
   {
     Serial.println("_________________________________");
     Serial.print("Upper Sensor Distance (mm): ");
@@ -99,10 +109,10 @@ void loop()
     Serial.println("_________________________________");
     Serial.println("");
   } else {
-    Serial.println("TIMEOUT");
+    Serial.println("TIMEOUT 1");
   }
 
-  //FLT OR SENSOR2
+  // Sensor 2
   if (!sensor2.timeoutOccurred())
   {
     Serial.println("_________________________________");
@@ -120,6 +130,7 @@ void loop()
   Serial.println();
   Serial.println();
 
+  // Algorithm for lowering
   if (upperSensorDistance > 5000 && lowerSensorDistance < 60) {
     Serial.println("Lowering");
     digitalWrite(DIRECTION_UP_PIN, LOW);
